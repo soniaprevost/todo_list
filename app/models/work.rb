fad3c539d28a67f4e39337ea0668847bb920b793
soniaprevost <so.prevost@gmail.com>
2015-03-23 22:39:26 +0100

    starting front

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,10 +1,11 @@
-require 'benchmark'
-
 class Work < ActiveRecord::Base
   belongs_to :user
+  extend Enumerize
+
 
   validates :description, presence: :true
   validates :user_id, presence: :true
+  enumerize :project, in: %w(personal professional).sort
 
   def time_taken(work)
     ((work.updated_at - work.created_at)/60000)
